338f5f8697314be284dc081dff45562c8fb81205
unknown <ms568203@gmail.com>
2020-04-18 19:31:58 +0530

    Refactor. Added Usecase2-flip the coin mulitple time and calculate the singlet percentage

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,26 @@
 #!/bin/bash -x
+declare -A singletDictionary;
+read -p "Enter how many time you want to flip the coin: " numberOfTimes;
 head=0;
 tail=1;
-flipcoin=$((RANDOM%2));
-if (( $flipcoin == $head ))
-then
-	echo "Head";
-else
-	echo "Tail";
-fi
+headCount=0;
+tailCount=0;
+counter=0;
+for (( iteration=1; iteration<=numberOfTimes; iteration++ ))
+do
+	flipcoin=$((RANDOM%2));
+	((counter++));
+	if (( $flipcoin == $head ))
+	then
+		singletDictionary["$counter"]="Head";
+		(( headCount++ ));
+	else
+		singletDictionary["$counter"]="Tail";
+		(( tailCount++ ));
+	fi
+done
+
+echo ${singletDictionary[@]};
+echo "Singlet Combination: "
+echo "Head Percentage : "$(awk "BEGIN {printf $headCount/$numberOfTimes}");
+echo "Tail percentage : "$(awk "BEGIN {printf $tailCount/$numberOfTimes}");
